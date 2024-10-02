d4d156ffc04cf84905f4a5f289038385b4906c65
MariaFernandaCintra <mariafernandaccintra@gmail.com>
2024-10-02 14:35:17 -0300

    relação com o Msql

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express"); //Importa o módulo Express
 
+const testeConnect = require("./db/testConnect");
+
 class AppController {
   //Define uma classe para organizar a lógica da aplicação
   constructor() {
     this.express = express(); //Cria uma nova instância do Express dentro da classe
     this.middlewares(); //Chama o método middlewares para configurar os middlewares
     this.routes(); //Chama o método routes para definir as rotas da API
+    testeConnect();
   }
 
   middlewares() {
